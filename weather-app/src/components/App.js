382d8f0acea569593076f6458e42973aa2919ef8
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2023-11-01 14:38:01 +0100

    fix API

--- weather-app/src/components/App.js
+++ weather-app/src/components/App.js
@@ -1,27 +1,29 @@
-import '../styles/App.css';
-import Header from './Header';
-import Weather from './Weather';
-import { useState, useEffect } from 'react';
+import "../styles/App.css";
+import Header from "./Header";
+import Weather from "./Weather";
+import { useState, useEffect } from "react";
 
 function App() {
-
-  const [city, setCity] = useState('Lyon');
+  const [city, setCity] = useState("Lyon");
   const [data, setData] = useState([]);
-  
+  const apiUrl = process.env.REACT_APP_WEATHER_API_URL;
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+
   useEffect(() => {
     const fetchWeatherData = () => {
-      fetch(`${process.env.REACT_APP_WEATHER_API_URL}/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${city}&days=5&aqi=no&alerts=no`)
-        .then(response => {
-          return response.json()
+      fetch(
+        `${apiUrl}?key=${apiKey}&q=${city}&days=5&aqi=no&alerts=no`
+      )
+        .then((response) => {
+          return response.json();
         })
-        .then(data => {
+        .then((data) => {
           setData(data);
           setCity(data.location.name);
-        })
+        });
     };
-    fetchWeatherData()
-  }, [city])
-
+    fetchWeatherData();
+  }, [city]);
 
   return (
     <div className="App">
@@ -29,18 +31,13 @@ function App() {
 
       <div className="row">
         <div className="col s12 m6 push-m3">
-
-        {(typeof data.location != 'undefined') ? (
-          <Weather city={city} weatherData={data}/>
-        ): (
-          <div></div>
-        )}
-
-          
-              
+          {typeof data.location != "undefined" ? (
+            <Weather city={city} weatherData={data} />
+          ) : (
+            <div></div>
+          )}
         </div>
       </div>
-
     </div>
   );
 }
